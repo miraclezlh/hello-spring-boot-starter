796d81c1621a62278f01d6de4d31f8e85f7ad719
season <706350462@qq.com>
2025-06-04 21:58:13 +0800

    增加Google Guava限流

--- src/main/java/com/zlh/hello_spring_boot_starter/controller/HelloController.java
+++ src/main/java/com/zlh/hello_spring_boot_starter/controller/HelloController.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -13,6 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
+    /**
+     * 限流策略 ： 1秒钟2个请求
+     */
+    private final RateLimiter limiter = RateLimiter.create(2.0);
+
+    private final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+
     @Autowired
     private HelloService helloService;
 
@@ -20,5 +33,16 @@ public class HelloController {
     @GetMapping("/demo")
     public void  home() {
         helloService.send("starter come in!");
+
+        //500毫秒内，没拿到令牌，就直接进入服务降级
+        boolean tryAcquire = limiter.tryAcquire(500, TimeUnit.MILLISECONDS);
+        if (!tryAcquire) {
+            System.out.println("进入服务降级，时间{}"+LocalDateTime.now().format(dtf));
+            System.out.println("当前排队人数较多，请稍后再试！");
+            return;
+        }
+
+        System.out.println("获取令牌成功，时间{}"+LocalDateTime.now().format(dtf));
+        System.out.println("请求成功");
     }
 }
